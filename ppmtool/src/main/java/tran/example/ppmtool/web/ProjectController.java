c7dc175c6346f755be18f0502244212563cb0552
jstran9 <wptran58@gmail.com>
2019-06-14 09:39:21 -0700

    removing unused imports

--- ppmtool/src/main/java/tran/example/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/tran/example/ppmtool/web/ProjectController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +13,6 @@ import tran.example.ppmtool.services.ProjectService;
 import tran.example.ppmtool.services.validations.MapValidationErrorService;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
