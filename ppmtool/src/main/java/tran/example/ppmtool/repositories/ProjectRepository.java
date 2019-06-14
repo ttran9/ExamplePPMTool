b35c9bc11de27820b7a103f2d0b7047dbf19ee64
jstran9 <wptran58@gmail.com>
2019-06-14 07:52:29 -0700

    creating a repository to access the Project table.

---
+++ ppmtool/src/main/java/tran/example/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package tran.example.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import tran.example.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
