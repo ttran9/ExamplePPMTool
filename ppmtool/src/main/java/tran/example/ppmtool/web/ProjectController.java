987b0eb96af7e900fd5bdc050cccd1d88b2150a3
jstran9 <wptran58@gmail.com>
2019-06-14 08:42:50 -0700

    modifying createNewProject to return a generic object in the ResponseEntity and returning a custom error.

--- ppmtool/src/main/java/tran/example/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/tran/example/ppmtool/web/ProjectController.java
@@ -3,12 +3,15 @@ package tran.example.ppmtool.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tran.example.ppmtool.domain.Project;
 import tran.example.ppmtool.services.ProjectService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,7 +25,13 @@ public class ProjectController {
     }
 
     @PostMapping
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
+        // we should return a generic type so we can return a ResponseEntity with more than just Project.
+
+        if(bindingResult.hasErrors()) {
+            return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
+
         Project project1 = this.projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project1, HttpStatus.CREATED);
     }
