d49383a0b485c4b9f48f739a97ba813468edacba
jstran9 <wptran58@gmail.com>
2019-06-16 09:55:54 -0700

    modifying the URLs.

--- ppmtool-react-client/src/Constants.js
+++ ppmtool-react-client/src/Constants.js
@@ -1,3 +1,3 @@
 export const BASE_URL =
-  process.env.MANAGEMENT_BASE_URL || "http://localhost:8085/";
-export const CREATE_PROJECT_URL = `${BASE_URL}api/project`;
+  process.env.MANAGEMENT_BASE_URL || "http://localhost:8085/api/project";
+export const GET_ALL_PROJECTS = `${BASE_URL}/all`;
