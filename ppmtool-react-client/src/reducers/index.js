43db1735973df912d9603904afa9f566d018e02a
jstran9 <wptran58@gmail.com>
2019-06-15 22:22:10 -0700

    bring in errors to our root reducer.

--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import errorReducer from "./errorReducer";
 
 export default combineReducers({
   // returns all the reducers
+
+  errors: errorReducer
 });
