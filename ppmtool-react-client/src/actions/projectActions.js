ecd31fb1e5ea47a199c63dd75cdd531ce40e3832
jstran9 <wptran58@gmail.com>
2019-06-16 09:56:13 -0700

    adding an action to get our projects.

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
 import * as Constants from "../Constants";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const response = await axios.post(Constants.CREATE_PROJECT_URL, project);
+    const response = await axios.post(Constants.BASE_URL, project);
     // take us back to the dashboard if we succesfully create the project.
     history.push("/");
   } catch (error) {
@@ -13,4 +13,12 @@ export const createProject = (project, history) => async dispatch => {
       payload: error.response.data
     });
   }
+};
+
+export const getProjects = () => async dispatch => {
+  const response = await axios.get(Constants.GET_ALL_PROJECTS);
+  dispatch({
+    type: GET_PROJECTS,
+    payload: response.data
+  });
 };
