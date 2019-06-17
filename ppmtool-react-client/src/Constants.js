dc11c34f38b270ae91f929244b44de1fdd0fa56f
jstran9 <wptran58@gmail.com>
2019-06-16 19:41:09 -0700

    adding a URL to redirect to the dashboard.

--- ppmtool-react-client/src/Constants.js
+++ ppmtool-react-client/src/Constants.js
@@ -2,3 +2,4 @@ export const BASE_URL =
   process.env.MANAGEMENT_BASE_URL || "http://localhost:8085/api/project";
 export const GET_ALL_PROJECTS = `${BASE_URL}/all`;
 export const GET_PROJECT = `${BASE_URL}/`;
+export const DASHBOARD_URL = "/dashboard";
