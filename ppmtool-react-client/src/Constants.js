a2493312afb20201027b5dca06904eefa80160e2
jstran9 <wptran58@gmail.com>
2019-06-16 18:59:13 -0700

    adding a url to retrieve a project.

--- ppmtool-react-client/src/Constants.js
+++ ppmtool-react-client/src/Constants.js
@@ -1,3 +1,4 @@
 export const BASE_URL =
   process.env.MANAGEMENT_BASE_URL || "http://localhost:8085/api/project";
 export const GET_ALL_PROJECTS = `${BASE_URL}/all`;
+export const GET_PROJECT = `${BASE_URL}/`;
