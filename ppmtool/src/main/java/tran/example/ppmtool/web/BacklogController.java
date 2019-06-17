2f64cc32273ae7b15e8c475a84419cdb212a983f
jstran9 <wptran58@gmail.com>
2019-06-17 16:25:58 -0700

    handling the mapping to get all the project tasks assoicated with a specific Backlog.

--- ppmtool/src/main/java/tran/example/ppmtool/web/BacklogController.java
+++ ppmtool/src/main/java/tran/example/ppmtool/web/BacklogController.java
@@ -4,17 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tran.example.ppmtool.domain.ProjectTask;
 import tran.example.ppmtool.services.projecttasks.ProjectTaskService;
 import tran.example.ppmtool.services.validations.MapValidationErrorService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/backlog")
@@ -40,5 +36,10 @@ public class BacklogController {
         ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
 
         return new ResponseEntity<>(projectTask1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{backlog_id}")
+    public Iterable<ProjectTask> getProjectBackLog(@PathVariable String backlog_id) {
+        return projectTaskService.findBacklogById(backlog_id);
     }
 }
