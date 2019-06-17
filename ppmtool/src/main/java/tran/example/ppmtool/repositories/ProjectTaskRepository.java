d0d3e912e7024de55e1e30f8a85b8eb34e563224
jstran9 <wptran58@gmail.com>
2019-06-17 16:26:31 -0700

    declaring a method to be implemented at run time to get a list of project tasks ordered by priority.

--- ppmtool/src/main/java/tran/example/ppmtool/repositories/ProjectTaskRepository.java
+++ ppmtool/src/main/java/tran/example/ppmtool/repositories/ProjectTaskRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import tran.example.ppmtool.domain.ProjectTask;
 
+import java.util.List;
+
 @Repository
 public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+
+    List<ProjectTask> findByProjectIdentifierOrderByPriority(String projectIdentifier);
 }
