f6f535a2775028d6c562f6d21a7457ed7bdf304d
jstran9 <wptran58@gmail.com>
2019-06-17 13:34:38 -0700

    creating the Backlog table.

---
+++ ppmtool/src/main/java/tran/example/ppmtool/domain/Backlog.java
@@ -0,0 +1,46 @@
+package tran.example.ppmtool.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Backlog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer PTSequence = 0; // the sequence of project tasks within each back log.
+    private String projectIdentifier;
+
+    // OneToOne with project
+
+
+    // OneToMany projecttasks
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPTSequence() {
+        return PTSequence;
+    }
+
+    public void setPTSequence(Integer PTSequence) {
+        this.PTSequence = PTSequence;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+}
