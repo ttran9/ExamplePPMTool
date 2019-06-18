303889c12a389543135600bad2ed35fd169a49f3
jstran9 <wptran58@gmail.com>
2019-06-18 13:20:28 -0700

    removing unused import.

--- ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-// import Backlog from "./Backlog";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getBacklog } from "../../actions/backlogActions";
