f860ac409b5aa1bf4274e20918e4d9672a9bcaca
jstran9 <wptran58@gmail.com>
2019-06-17 22:23:58 -0700

    adding ProjectBoard URL.

--- ppmtool-react-client/src/Constants.js
+++ ppmtool-react-client/src/Constants.js
@@ -1,3 +1,4 @@
 export const DASHBOARD_URL = "/dashboard";
-export const PROJECT_API_URL = `/api/project`;
-export const BACKLOG_API_URL = `/api/backlog`;
+export const PROJECTBOARD_URL = "/projectBoard";
+export const PROJECT_API_URL = "/api/project";
+export const BACKLOG_API_URL = "/api/backlog";
