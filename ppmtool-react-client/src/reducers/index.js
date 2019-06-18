cfa5fe2473a74f06906afb8d464ea69b8547a328
jstran9 <wptran58@gmail.com>
2019-06-17 19:51:37 -0700

    wiring our backlog reducer to our root reducer.

--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
+import backlogReducer from "./backlogReducer";
 
 export default combineReducers({
   // returns all the reducers
 
   errors: errorReducer,
-  project: projectReducer
+  project: projectReducer,
+  backlog: backlogReducer
 });
