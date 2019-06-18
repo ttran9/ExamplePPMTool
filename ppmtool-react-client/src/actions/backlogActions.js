d81ea5dfc419838bb9320d8f6abc8b6e92d9328a
jstran9 <wptran58@gmail.com>
2019-06-17 23:10:49 -0700

    populate the payload with errors if any or empty the errors if successful submit after making the project task submission.

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 import * as Constants from "../Constants";
+import { GET_ERRORS } from "./types";
 
 export const addProjectTask = (
   backlogId,
   projectTask,
   history
 ) => async dispatch => {
-  await axios.post(`${Constants.BACKLOG_API_URL}/${backlogId}`, projectTask);
-  history.push(`${Constants.PROJECTBOARD_URL}/${backlogId}`);
+  try {
+    await axios.post(`${Constants.BACKLOG_API_URL}/${backlogId}`, projectTask);
+    history.push(`${Constants.PROJECTBOARD_URL}/${backlogId}`);
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
 };
