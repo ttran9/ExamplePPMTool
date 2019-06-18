d0bf1b9fc23c18dc39270646aac7b8c2a2b6c8c5
jstran9 <wptran58@gmail.com>
2019-06-18 12:23:53 -0700

    adding status related constants.

--- ppmtool-react-client/src/Constants.js
+++ ppmtool-react-client/src/Constants.js
@@ -2,3 +2,6 @@ export const DASHBOARD_URL = "/dashboard";
 export const PROJECTBOARD_URL = "/projectBoard";
 export const PROJECT_API_URL = "/api/project";
 export const BACKLOG_API_URL = "/api/backlog";
+export const TO_DO_STATUS = "TO_DO";
+export const IN_PROGRESS_STATUS = "IN_PROGRESS";
+export const DONE_STATUS = "DONE";
