bbebf2c1bbaabb7db3be6b540aa479350e849133
jstran9 <wptran58@gmail.com>
2019-06-17 17:18:57 -0700

    moving to different package.

--- ppmtool/src/main/java/tran/example/ppmtool/exceptions/projects/ProjectNotFoundException.java
+++ ppmtool/src/main/java/tran/example/ppmtool/exceptions/projects/ProjectNotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class ProjectIdException extends RuntimeException {
+public class ProjectNotFoundException extends RuntimeException {
 
-    public ProjectIdException(String message) {
+    public ProjectNotFoundException(String message) {
         super(message);
     }
 }
