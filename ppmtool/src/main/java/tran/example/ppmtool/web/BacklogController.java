f35a2eca2d1d549917b53622fcbf6d84f4da51d8
jstran9 <wptran58@gmail.com>
2019-06-17 19:00:07 -0700

    removing unused important.

--- ppmtool/src/main/java/tran/example/ppmtool/web/BacklogController.java
+++ ppmtool/src/main/java/tran/example/ppmtool/web/BacklogController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import tran.example.ppmtool.domain.Project;
 import tran.example.ppmtool.domain.ProjectTask;
 import tran.example.ppmtool.services.projecttasks.ProjectTaskService;
 import tran.example.ppmtool.services.validations.MapValidationErrorService;
