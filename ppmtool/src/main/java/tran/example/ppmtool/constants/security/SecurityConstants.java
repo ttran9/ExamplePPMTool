e2d9d8b16d986f2ab32b1d754d82228a5d34234a
jstran9 <wptran58@gmail.com>
2019-06-19 11:41:32 -0700

    hard-coding secret key but will re-factor later down the line.

--- ppmtool/src/main/java/tran/example/ppmtool/constants/security/SecurityConstants.java
+++ ppmtool/src/main/java/tran/example/ppmtool/constants/security/SecurityConstants.java
@@ -1,13 +1,10 @@
 package tran.example.ppmtool.constants.security;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public class SecurityConstants {
 
     public static final String SIGN_UP_URLS = "/api/users/**";
     public static final String H2_URL = "/h2-console/**";
-    @Value("${JWTSuperSecretKey}")
-    public static final String SECRET = ""; // TODO: will refactor this with a more secure implementation
+    public static final String SECRET = "Secret"; // TODO: will refactor this with a more secure implementation
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final long EXPIRATION_TIME = 30000; // in milliseconds so 30 seconds.
