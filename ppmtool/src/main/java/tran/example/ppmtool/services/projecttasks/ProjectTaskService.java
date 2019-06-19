0ce48f1156bc76d3aa8a5cdce9d2405919b10b89
jstran9 <wptran58@gmail.com>
2019-06-19 16:15:08 -0700

    adding a principal parameter to each method.

--- ppmtool/src/main/java/tran/example/ppmtool/services/projecttasks/ProjectTaskService.java
+++ ppmtool/src/main/java/tran/example/ppmtool/services/projecttasks/ProjectTaskService.java
@@ -27,23 +27,27 @@ public interface ProjectTaskService {
      * Gets the project task with the specified project sequence and is a part of the proper backlog.
      * @param backlogId The backlog identifier that the project task is a part of.
      * @param projectSequence The project sequence identifying a project task.
+     * @param principal The object expected to hold the logged in user's information (such as username).
      * @return Returns a project task.
      */
-    ProjectTask findProjectTaskByBackLogIdAndProjectSequence(String backlogId, String projectSequence);
+    ProjectTask findProjectTaskByBackLogIdAndProjectSequence(String backlogId, String projectSequence, Principal principal);
 
     /**
      * Updates the project task with the specified project sequence and backlogId.
      * @param updatedProjectTask The project task with the newly updated content(s).
      * @param backlogId The backlog Id that this project task is a part of.
      * @param projectSequence The project sequence of the project task to be updated.
+     * @param principal The object expected to hold the logged in user's information (such as username).
      * @return Returns the updated project task.
      */
-    ProjectTask updateProjectTaskByProjectSequenceAndBacklogId(ProjectTask updatedProjectTask, String backlogId, String projectSequence);
+    ProjectTask updateProjectTaskByProjectSequenceAndBacklogId(ProjectTask updatedProjectTask, String backlogId,
+                                                               String projectSequence, Principal principal);
 
     /**
      * Deletes a project task with the specified project sequence and backlogId.
      * @param backlogId The backlog Id that this project task is a part of.
      * @param projectSequence The project sequence of the project task to be removed.
+     * @param principal The object expected to hold the logged in user's information (such as username).
      */
-    void deleteProjectTaskByProjectSequenceAndBacklogId(String backlogId, String projectSequence);
+    void deleteProjectTaskByProjectSequenceAndBacklogId(String backlogId, String projectSequence, Principal principal);
 }
