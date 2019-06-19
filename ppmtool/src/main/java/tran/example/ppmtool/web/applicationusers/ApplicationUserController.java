10135c5e655af4fa1fdf17465f7aa79896538db1
jstran9 <wptran58@gmail.com>
2019-06-19 12:13:21 -0700

    process the user login and generate the token if credentials are valid.

--- ppmtool/src/main/java/tran/example/ppmtool/web/applicationusers/ApplicationUserController.java
+++ ppmtool/src/main/java/tran/example/ppmtool/web/applicationusers/ApplicationUserController.java
@@ -3,12 +3,20 @@ package tran.example.ppmtool.web.applicationusers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tran.example.ppmtool.config.security.JwtTokenProvider;
+import tran.example.ppmtool.constants.security.SecurityConstants;
 import tran.example.ppmtool.domain.applicationuser.ApplicationUser;
+import tran.example.ppmtool.payload.JWTLoginSuccessResponse;
+import tran.example.ppmtool.payload.LoginRequest;
 import tran.example.ppmtool.services.applicationusers.ApplicationUserService;
 import tran.example.ppmtool.services.validations.MapValidationErrorService;
 import tran.example.ppmtool.validator.ApplicationUserValidator;
@@ -25,12 +33,40 @@ public class ApplicationUserController {
 
     private ApplicationUserValidator applicationUserValidator;
 
+    private JwtTokenProvider jwtTokenProvider;
+
+    private AuthenticationManager authenticationManager;
+
+
     @Autowired
     public ApplicationUserController(ApplicationUserService applicationUserService, MapValidationErrorService mapValidationErrorService,
-                                     ApplicationUserValidator applicationUserValidator) {
+                                     ApplicationUserValidator applicationUserValidator, JwtTokenProvider jwtTokenProvider,
+                                     AuthenticationManager authenticationManager) {
         this.applicationUserService = applicationUserService;
         this.mapValidationErrorService = mapValidationErrorService;
         this.applicationUserValidator = applicationUserValidator;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult bindingResult) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.outputCustomError(bindingResult);
+        if(errorMap != null) return errorMap;
+
+
+        // TODO: refactor this to a service later...
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwtToken = SecurityConstants.TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwtToken));
     }
 
     @PostMapping("/register")
