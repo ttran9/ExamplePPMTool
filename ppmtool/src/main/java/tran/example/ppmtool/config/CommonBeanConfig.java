624ca00d87dd4eb0ca7973735417c733f5f08e12
jstran9 <wptran58@gmail.com>
2019-06-19 00:07:00 -0700

    removing unused imports.

--- ppmtool/src/main/java/tran/example/ppmtool/config/CommonBeanConfig.java
+++ ppmtool/src/main/java/tran/example/ppmtool/config/CommonBeanConfig.java
@@ -4,13 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableJpaRepositories("tran.example.ppmtool.repositories")
