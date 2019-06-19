31fca443fdadee65a4a2189b7f8da9d18ff6245b
jstran9 <wptran58@gmail.com>
2019-06-19 13:51:59 -0700

    updating the token to expire in 5 minutes instead of 30 seconds.

--- ppmtool/src/main/java/tran/example/ppmtool/constants/security/SecurityConstants.java
+++ ppmtool/src/main/java/tran/example/ppmtool/constants/security/SecurityConstants.java
@@ -7,6 +7,6 @@ public class SecurityConstants {
     public static final String SECRET = "Secret"; // TODO: will refactor this with a more secure implementation
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 30000; // in milliseconds so 30 seconds.
+    public static final long EXPIRATION_TIME = 300000; // in milliseconds so 300 seconds / 5mins.
 
 }
