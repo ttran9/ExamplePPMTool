bbc466fbf96e7d75b868f8ca52b08fe7449802ed
jstran9 <wptran58@gmail.com>
2019-06-19 11:34:52 -0700

    setting up configuration to be able to generate JWT token.

--- ppmtool/src/main/java/tran/example/ppmtool/config/security/SecurityConfig.java
+++ ppmtool/src/main/java/tran/example/ppmtool/config/security/SecurityConfig.java
@@ -1,13 +1,20 @@
 package tran.example.ppmtool.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import tran.example.ppmtool.constants.security.SecurityConstants;
+import tran.example.ppmtool.services.security.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -20,9 +27,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private AuthenticationEntryPoint authenticationEntryPoint;
 
+    private CustomUserDetailsService customUserDetailsService;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Autowired
-    public SecurityConfig(AuthenticationEntryPoint authenticationEntryPoint) {
+    public SecurityConfig(AuthenticationEntryPoint authenticationEntryPoint, CustomUserDetailsService customUserDetailsService,
+                          BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.authenticationEntryPoint = authenticationEntryPoint;
+        this.customUserDetailsService = customUserDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     @Override
@@ -46,7 +71,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js"
                 ).permitAll()
-                .antMatchers("/api/users/**").permitAll()
+                .antMatchers(SecurityConstants.SIGN_UP_URLS).permitAll()
+                .antMatchers(SecurityConstants.H2_URL).permitAll()
                 .anyRequest().authenticated();
     }
 }
