b564001c2c4fbaa7682d3cfd096d1d82d3d380dd
jstran9 <wptran58@gmail.com>
2019-06-20 09:48:26 -0700

    adding logic to set the jwt token in our main App component.

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -15,6 +15,29 @@ import Landing from "./components/Layout/Landing";
 import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
 import * as Constants from "./Constants";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_token = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_token
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decoded_token.exp < currentTime) {
+    console.log("token has expired...");
+    // handle logout
+    // window.location.href = `${Constants.HOME_URL}`;
+  } else {
+    console.log("token not yet expired..");
+  }
+}
 
 class App extends Component {
   render() {
