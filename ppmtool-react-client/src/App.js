ef72491c3ca708acfc204af227ab168598e6224a
jstran9 <wptran58@gmail.com>
2019-06-19 21:22:05 -0700

    adding routes for the registration, login, and dashboard pages.

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -11,6 +11,10 @@ import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/UserManagement/Register";
+import Login from "./components/UserManagement/Login";
+import * as Constants from "./Constants";
 
 class App extends Component {
   render() {
@@ -19,7 +23,24 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
-            <Route exact path="/dashboard" component={Dashboard} />
+            {
+              // Public Routes
+            }
+            <Route exact path={`${Constants.HOME_URL}`} component={Landing} />
+            <Route
+              exact
+              path={`${Constants.REGISTER_URL}`}
+              component={Register}
+            />
+            <Route exact path={`${Constants.LOGIN_URL}`} component={Login} />
+            {
+              // Private Routes
+            }
+            <Route
+              exact
+              path={`${Constants.DASHBOARD_URL}`}
+              component={Dashboard}
+            />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
             <Route exact path="/projectBoard/:id" component={ProjectBoard} />
