d0404bf4da5750cce79dba26c40a34c7e6ce4e1c
jstran9 <wptran58@gmail.com>
2019-06-19 23:06:09 -0700

    creating the action to log the user in.

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import * as Constants from "../Constants";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatch => {
   try {
@@ -9,6 +11,37 @@ export const createNewUser = (newUser, history) => async dispatch => {
     dispatch({
       type: GET_ERRORS,
       payload: {}
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+
+export const login = LoginRequest => async dispatch => {
+  try {
+    // post login endpoint with LoginRequest payload object
+    const response = await axios.post(
+      `${Constants.LOGIN_ENDPOINT}`,
+      LoginRequest
+    );
+    // extract token from the response.data
+    const { token } = response.data;
+    // store the token in the localStorage
+    localStorage.setItem("jwtToken", token);
+
+    // set our token in the header (Authorization: Bearer [token here])
+    setJWTToken(token);
+
+    // decode token on React
+    const decoded = jwt_decode(token);
+
+    // dispatch to our securityReducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded
     });
   } catch (error) {
     dispatch({
